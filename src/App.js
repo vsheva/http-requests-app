b14d3b073b4a24824a2fb947942ba56141449305
vsheva <valera.sheva@gmail.com>
2022-07-13 02:00:13 +0300

    add loading information

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import './App.css';
 function App() {
 
   const [movies, setMovies]= useState([])
+  const [isLoading, setIsLoading]= useState(false)
 
 
     const fetchMoviesHandler =async()=>{
+        setIsLoading(true) //*
          const response= await fetch("https://swapi.dev/api/films")
             const data = await response.json();
 
@@ -21,8 +23,8 @@ function App() {
                          //setMovies(data.results)
                      }
                  });
-                 setMovies(transformedMovies)
-
+                 setMovies(transformedMovies)  //*
+        setIsLoading(false)
      }
 
 
@@ -67,7 +69,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+          {!isLoading &&  movies.length>0 && <MoviesList movies={movies}/>}
+          {!isLoading && movies.length===0 && <p>No films found</p>}
+          {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
